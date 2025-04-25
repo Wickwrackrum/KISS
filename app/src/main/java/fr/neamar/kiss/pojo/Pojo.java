099667da60c96266e29b06f45776bddd2e4a996f
Wickwrackrum <sciphi1618@gmail.com>
2025-04-25 17:35:30 +0530

    Update Pojo.java

--- app/src/main/java/fr/neamar/kiss/pojo/Pojo.java
+++ app/src/main/java/fr/neamar/kiss/pojo/Pojo.java
@@ -5,6 +5,7 @@ import fr.neamar.kiss.utils.fuzzy.MatchInfo;
 
 public abstract class Pojo {
     public static final String DEFAULT_ID = "(none)";
+    public static final int FOLDER = 99; // Add this at the top with the others
 
     // Globally unique ID.
     // Usually starts with provider scheme, e.g. "app://" or "contact://" to
@@ -92,3 +93,4 @@ public abstract class Pojo {
         return false;
     }
 }
+
